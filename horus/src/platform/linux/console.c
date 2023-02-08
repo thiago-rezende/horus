f06efe3dbe14b3659dbf2a6a0a3e410b5504c813
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2023-02-08 19:25:44 -0300

    feat(logger): flush the streams after message write

--- horus/src/platform/linux/console.c
+++ horus/src/platform/linux/console.c
@@ -5,11 +5,15 @@
 b8 platform_console_write(char* message) {
   u32 written = fprintf(stdout, "%s", message);
 
+  fflush(stdout);
+
   return written < 0 ? false : true;
 }
 
 b8 platform_console_write_error(char* message) {
   u32 written = fprintf(stderr, "%s", message);
+
+  fflush(stderr);
 
   return written < 0 ? false : true;
 }
