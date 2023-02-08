eafea9392b0ea7a8769cc06e60726835d04386d2
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2023-02-07 22:20:20 -0300

    style: add .editorconfig file

--- horus/include/horus/platform/linux/entrypoint.h
+++ horus/include/horus/platform/linux/entrypoint.h
@@ -8,6 +8,7 @@ int main(int argc, char** argv, char** envp) {
   platform_console_write("[ horus ] <linux> v0.1.0\n");
 
   void* application = application_create();
+
   (void)application; /* unused */
 
   platform_console_write("[ application ] created\n");
