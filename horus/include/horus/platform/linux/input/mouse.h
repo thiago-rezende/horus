ee34dee638bb17fdb1ccda92ea5bf3ecf36e887a
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-05-26 16:55:02 -0300

    fix(input|linux): add missing parameters on mouse scroll state setter function signature

--- horus/include/horus/platform/linux/input/mouse.h
+++ horus/include/horus/platform/linux/input/mouse.h
@@ -15,7 +15,7 @@ mouse_position_t __platform_input_mouse_previous_position();
 b8 __platform_input_mouse_set_current_position(mouse_position_t position);
 b8 __platform_input_mouse_set_previous_position(mouse_position_t position);
 
-b8 __platform_input_mouse_scroll_set_state();
+b8 __platform_input_mouse_scroll_set_state(mouse_scroll_state_t state);
 b8 __platform_input_mouse_button_set_state(mouse_button_t button, mouse_button_state_t state);
 
 b8 __platform_input_mouse_button_clear_state();
