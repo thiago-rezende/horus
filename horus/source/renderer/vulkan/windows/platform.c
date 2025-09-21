90197fa93e608d841d2b048a604322e0a545f84c
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-09-21 19:19:23 -0300

    refactor(renderer|vulkan): improved the platform extensions and add some device related extensions

--- horus/source/renderer/vulkan/windows/platform.c
+++ horus/source/renderer/vulkan/windows/platform.c
@@ -16,17 +16,27 @@
 #define VULKAN_INSTANCE_LAYERS_COUNT 1
 #define VULKAN_INSTANCE_EXTENSIONS_COUNT 3
 
-#define VULKAN_DEVICE_EXTENSIONS_COUNT 1
+#define VULKAN_DEVICE_EXTENSIONS_COUNT 4
 
 #define VK_KHRONOS_VALIDATION_LAYER_NAME "VK_LAYER_KHRONOS_validation"
 
+/* instance layers */
 const char *vk_khronos_validation_layer_name = VK_KHRONOS_VALIDATION_LAYER_NAME;
 
-const char *vk_khr_surface_extension_name = VK_KHR_SURFACE_EXTENSION_NAME;
-const char *vk_khr_swapchain_extension_name = VK_KHR_SWAPCHAIN_EXTENSION_NAME;
+/* instance debug extensions */
+const char *vk_ext_debug_utils_extension_name = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+
+/* instance platform extensions */
 const char *vk_khr_win32_surface_extension_name = VK_KHR_WIN32_SURFACE_EXTENSION_NAME;
 
-const char *vk_ext_debug_utils_extension_name = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+/* instance extensions */
+const char *vk_khr_surface_extension_name = VK_KHR_SURFACE_EXTENSION_NAME;
+
+/* device extensions */
+const char *vk_khr_spirv_1_4_extension_name = VK_KHR_SPIRV_1_4_EXTENSION_NAME;
+const char *vk_khr_swapchain_extension_name = VK_KHR_SWAPCHAIN_EXTENSION_NAME;
+const char *vk_khr_synchronization_2_extension_name = VK_KHR_SYNCHRONIZATION_2_EXTENSION_NAME;
+const char *vk_khr_create_renderpass_2_extension_name = VK_KHR_CREATE_RENDERPASS_2_EXTENSION_NAME;
 
 array_t *renderer_vulkan_instance_get_required_layers(void) {
   array_t *array = array_create(VULKAN_INSTANCE_LAYERS_COUNT, sizeof(char *));
@@ -50,7 +60,10 @@ array_t *renderer_vulkan_instance_get_required_extensions(void) {
 array_t *renderer_vulkan_device_get_required_extensions(void) {
   array_t *array = array_create(VULKAN_DEVICE_EXTENSIONS_COUNT, sizeof(char *));
 
+  array_insert(array, &vk_khr_spirv_1_4_extension_name);
   array_insert(array, &vk_khr_swapchain_extension_name);
+  array_insert(array, &vk_khr_synchronization_2_extension_name);
+  array_insert(array, &vk_khr_create_renderpass_2_extension_name);
 
   return array;
 }
