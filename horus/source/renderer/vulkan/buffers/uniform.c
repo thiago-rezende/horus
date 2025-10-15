44a7ff72230f4665b361bed1ac7a4965b7910de8
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-10-15 08:40:32 -0300

    feat(renderer|vulkan): update uniform buffer to support multiple frames in flight

--- horus/source/renderer/vulkan/buffers/uniform.c
+++ horus/source/renderer/vulkan/buffers/uniform.c
@@ -19,14 +19,13 @@
 struct __uniform_buffer {
   VkDevice device;
 
-  /* TODO: improve to support the frames in flight */
-  VkBuffer buffer;
-
-  /* TODO: improve to support the frames in flight */
   VkDeviceSize size;
-  VkDeviceMemory memory;
 
-  void *mapped;
+  array_t *buffers;
+  array_t *memories;
+  array_t *mappings;
+
+  u8 *current_frame_in_flight_index;
 };
 
 /* TODO: refactor into multipe functions for usage within other types of buffer */
@@ -36,116 +35,122 @@ uniform_buffer_t *uniform_buffer_create(renderer_t *renderer, uniform_buffer_obj
   *buffer = (uniform_buffer_t){
       .size = sizeof(uniform_buffer_object_t),
       .device = renderer->device,
-      .mapped = NULL,
+      .current_frame_in_flight_index = &renderer->current_frame_in_flight_index,
   };
 
-  VkBufferCreateInfo buffer_create_info = (VkBufferCreateInfo){
-      .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
-      .flags = (VkBufferCreateFlags)0,
-      .size = buffer->size,
-      .usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
-      .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
-  };
+  buffer->buffers = array_create(RENDERER_VULKAN_FRAMES_IN_FLIGHT, sizeof(VkBuffer));
+  buffer->memories = array_create(RENDERER_VULKAN_FRAMES_IN_FLIGHT, sizeof(VkDeviceMemory));
+  buffer->mappings = array_create(RENDERER_VULKAN_FRAMES_IN_FLIGHT, sizeof(void *));
 
-  if (vkCreateBuffer(renderer->device, &buffer_create_info, NULL, &buffer->buffer) != VK_SUCCESS) {
-    logger_critical_format("<renderer:%p> buffer creation failed", renderer);
+  for (u8 i = 0; i < RENDERER_VULKAN_FRAMES_IN_FLIGHT; i++) {
+    void *current_mapped;
+    VkBuffer current_buffer;
+    VkDeviceMemory current_memory;
 
-    platform_memory_deallocate(buffer);
+    VkBufferCreateInfo buffer_create_info = (VkBufferCreateInfo){
+        .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .flags = (VkBufferCreateFlags)0,
+        .size = buffer->size,
+        .usage = VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+        .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
+    };
 
-    return NULL;
-  }
+    if (vkCreateBuffer(buffer->device, &buffer_create_info, NULL, &current_buffer) != VK_SUCCESS) {
+      logger_critical_format("<renderer:%p> buffer creation failed", renderer);
 
-  VkMemoryRequirements buffer_memory_requirements;
-  VkPhysicalDeviceMemoryProperties physical_device_memory_properties;
+      platform_memory_deallocate(buffer);
 
-  VkMemoryPropertyFlags buffer_memory_properties =
-      VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
-
-  vkGetBufferMemoryRequirements(buffer->device, buffer->buffer, &buffer_memory_requirements);
-
-  vkGetPhysicalDeviceMemoryProperties(renderer->physical_device, &physical_device_memory_properties);
-
-  u32 buffer_memory_type_index = 0;
-
-  b8 has_required_buffer_memory_type = false;
-
-  for (u32 i = 0; i < physical_device_memory_properties.memoryTypeCount; i++) {
-    if ((buffer_memory_requirements.memoryTypeBits & (1 << i)) &&
-        (physical_device_memory_properties.memoryTypes[i].propertyFlags & buffer_memory_properties) ==
-            buffer_memory_properties) {
-      buffer_memory_type_index = i;
-
-      has_required_buffer_memory_type = true;
-
-      break;
+      return NULL;
     }
+
+    array_insert(buffer->buffers, (void *)&current_buffer);
+
+    VkMemoryRequirements buffer_memory_requirements;
+    VkPhysicalDeviceMemoryProperties physical_device_memory_properties;
+
+    VkMemoryPropertyFlags buffer_memory_properties =
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+
+    vkGetBufferMemoryRequirements(buffer->device, current_buffer, &buffer_memory_requirements);
+
+    vkGetPhysicalDeviceMemoryProperties(renderer->physical_device, &physical_device_memory_properties);
+
+    u32 buffer_memory_type_index = 0;
+
+    b8 has_required_buffer_memory_type = false;
+
+    for (u32 i = 0; i < physical_device_memory_properties.memoryTypeCount; i++) {
+      if ((buffer_memory_requirements.memoryTypeBits & (1 << i)) &&
+          (physical_device_memory_properties.memoryTypes[i].propertyFlags & buffer_memory_properties) ==
+              buffer_memory_properties) {
+        buffer_memory_type_index = i;
+
+        has_required_buffer_memory_type = true;
+
+        break;
+      }
+    }
+
+    if (!has_required_buffer_memory_type) {
+      logger_critical_format("<renderer:%p> <buffer:%p> desired memory type for buffers creation not found", renderer,
+                             buffer);
+
+      uniform_buffer_destroy(buffer);
+
+      return NULL;
+    }
+
+    VkMemoryAllocateInfo buffer_memory_allocate_info = (VkMemoryAllocateInfo){
+        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
+        .allocationSize = buffer_memory_requirements.size,
+        .memoryTypeIndex = buffer_memory_type_index,
+    };
+
+    /* TODO: implement a memory allocator to allow more than 4096 allocations */
+    if (vkAllocateMemory(buffer->device, &buffer_memory_allocate_info, NULL, &current_memory) != VK_SUCCESS) {
+      logger_critical_format("<renderer:%p> <buffer:%p> buffer memory allocation failed", renderer, buffer);
+
+      uniform_buffer_destroy(buffer);
+
+      return NULL;
+    }
+
+    array_insert(buffer->memories, (void *)&current_memory);
+
+    if (vkBindBufferMemory(buffer->device, current_buffer, current_memory, 0) != VK_SUCCESS) {
+      logger_critical_format("<renderer:%p> <buffer:%p> staging buffer memory binding failed", renderer, buffer);
+
+      uniform_buffer_destroy(buffer);
+
+      return NULL;
+    }
+
+    if (vkMapMemory(buffer->device, current_memory, 0, buffer_create_info.size, 0, &current_mapped) != VK_SUCCESS) {
+      logger_critical_format("<renderer:%p> <buffer:%p> buffer memory mapping failed", renderer, buffer);
+
+      uniform_buffer_destroy(buffer);
+
+      return NULL;
+    }
+
+    array_insert(buffer->mappings, (void *)&current_mapped);
+
+    platform_memory_copy(current_mapped, object, sizeof(uniform_buffer_object_t));
   }
-
-  if (!has_required_buffer_memory_type) {
-    logger_critical_format("<renderer:%p> <buffer:%p> desired memory type for buffers creation not found", renderer,
-                           buffer);
-
-    vkDestroyBuffer(buffer->device, buffer->buffer, NULL);
-
-    platform_memory_deallocate(buffer);
-
-    return NULL;
-  }
-
-  VkMemoryAllocateInfo buffer_memory_allocate_info = (VkMemoryAllocateInfo){
-      .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO,
-      .allocationSize = buffer_memory_requirements.size,
-      .memoryTypeIndex = buffer_memory_type_index,
-  };
-
-  /* TODO: implement a memory allocator to allow more than 4096 allocations */
-  if (vkAllocateMemory(buffer->device, &buffer_memory_allocate_info, NULL, &buffer->memory) != VK_SUCCESS) {
-    logger_critical_format("<renderer:%p> <buffer:%p> buffer memory allocation failed", renderer, buffer);
-
-    vkDestroyBuffer(buffer->device, buffer->buffer, NULL);
-
-    platform_memory_deallocate(buffer);
-
-    return NULL;
-  }
-
-  if (vkBindBufferMemory(buffer->device, buffer->buffer, buffer->memory, 0) != VK_SUCCESS) {
-    logger_critical_format("<renderer:%p> <buffer:%p> staging buffer memory binding failed", renderer, buffer);
-
-    vkFreeMemory(buffer->device, buffer->memory, NULL);
-
-    vkDestroyBuffer(buffer->device, buffer->buffer, NULL);
-
-    platform_memory_deallocate(buffer);
-
-    return NULL;
-  }
-
-  if (vkMapMemory(buffer->device, buffer->memory, 0, buffer_create_info.size, 0, &buffer->mapped) != VK_SUCCESS) {
-    logger_critical_format("<renderer:%p> <buffer:%p> buffer memory mapping failed", renderer, buffer);
-
-    vkFreeMemory(buffer->device, buffer->memory, NULL);
-
-    vkDestroyBuffer(buffer->device, buffer->buffer, NULL);
-
-    platform_memory_deallocate(buffer);
-
-    return NULL;
-  }
-
-  platform_memory_copy(buffer->mapped, object, sizeof(uniform_buffer_object_t));
 
   return buffer;
 }
 
 b8 uniform_buffer_bind(uniform_buffer_t *buffer, graphics_pipeline_t *pipeline, renderer_t *renderer) {
-  VkCommandBuffer graphics_command_buffer;
+  VkBuffer current_buffer;
   VkDescriptorSet descriptor_set;
+  VkCommandBuffer graphics_command_buffer;
 
-  array_retrieve(renderer->graphics_command_buffers, renderer->current_frame_in_flight_index, &graphics_command_buffer);
+  array_retrieve(buffer->buffers, renderer->current_frame_in_flight_index, &current_buffer);
   array_retrieve(pipeline->descriptor_sets, renderer->current_frame_in_flight_index, &descriptor_set);
+  array_retrieve(renderer->graphics_command_buffers, renderer->current_frame_in_flight_index, &graphics_command_buffer);
 
-  renderer_vulkan_descriptor_set_update(renderer->device, descriptor_set, buffer->buffer,
+  renderer_vulkan_descriptor_set_update(renderer->device, descriptor_set, current_buffer,
                                         sizeof(uniform_buffer_object_t));
 
   vkCmdBindDescriptorSets(graphics_command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline->layout, 0, 1,
@@ -155,7 +160,11 @@ b8 uniform_buffer_bind(uniform_buffer_t *buffer, graphics_pipeline_t *pipeline, 
 }
 
 b8 uniform_buffer_update(uniform_buffer_t *buffer, uniform_buffer_object_t *object) {
-  platform_memory_copy(buffer->mapped, object, sizeof(uniform_buffer_object_t));
+  void *mapped = NULL;
+
+  array_retrieve(buffer->mappings, *buffer->current_frame_in_flight_index, &mapped);
+
+  platform_memory_copy(mapped, object, sizeof(uniform_buffer_object_t));
 
   return true;
 }
@@ -163,11 +172,29 @@ b8 uniform_buffer_update(uniform_buffer_t *buffer, uniform_buffer_object_t *obje
 b8 uniform_buffer_destroy(uniform_buffer_t *buffer) {
   vkDeviceWaitIdle(buffer->device);
 
-  vkUnmapMemory(buffer->device, buffer->memory);
+  array_destroy(buffer->mappings);
 
-  vkFreeMemory(buffer->device, buffer->memory, NULL);
+  for (u8 i = 0; i < buffer->memories->count; i++) {
+    VkDeviceMemory memory = VK_NULL_HANDLE;
 
-  vkDestroyBuffer(buffer->device, buffer->buffer, NULL);
+    array_retrieve(buffer->memories, i, (void *)&memory);
+
+    vkUnmapMemory(buffer->device, memory);
+
+    vkFreeMemory(buffer->device, memory, NULL);
+  }
+
+  array_destroy(buffer->memories);
+
+  for (u8 i = 0; i < buffer->buffers->count; i++) {
+    VkBuffer current_buffer = VK_NULL_HANDLE;
+
+    array_retrieve(buffer->buffers, i, (void *)&current_buffer);
+
+    vkDestroyBuffer(buffer->device, current_buffer, NULL);
+  }
+
+  array_destroy(buffer->buffers);
 
   platform_memory_deallocate(buffer);
 
