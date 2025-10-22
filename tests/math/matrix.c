3544ac661701e49859da67b6cdca5543b1ffb46a
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-10-22 12:08:52 -0300

    test(math): add matrix4f32 euler rotation function unit test

--- tests/math/matrix.c
+++ tests/math/matrix.c
@@ -164,8 +164,50 @@ static void test_matrix4f32_translate(void **state) {
   /* function call */
   matrix4f32_t result = matrix4f32_translate(identity, vector);
 
-  /* general assertion */
-  assert_memory_equal(&result, &expected, sizeof(matrix4f32_t));
+  /* union assertions */
+  assert_float_equal(result.x0, expected.x0, epsilon_f32);
+  assert_float_equal(result.y0, expected.y0, epsilon_f32);
+  assert_float_equal(result.z0, expected.z0, epsilon_f32);
+  assert_float_equal(result.w0, expected.w0, epsilon_f32);
+
+  assert_float_equal(result.x1, expected.x1, epsilon_f32);
+  assert_float_equal(result.y1, expected.y1, epsilon_f32);
+  assert_float_equal(result.z1, expected.z1, epsilon_f32);
+  assert_float_equal(result.w1, expected.w1, epsilon_f32);
+
+  assert_float_equal(result.x2, expected.x2, epsilon_f32);
+  assert_float_equal(result.y2, expected.y2, epsilon_f32);
+  assert_float_equal(result.z2, expected.z2, epsilon_f32);
+  assert_float_equal(result.w2, expected.w2, epsilon_f32);
+
+  assert_float_equal(result.x3, expected.x3, epsilon_f32);
+  assert_float_equal(result.y3, expected.y3, epsilon_f32);
+  assert_float_equal(result.z3, expected.z3, epsilon_f32);
+  assert_float_equal(result.w3, expected.w3, epsilon_f32);
+}
+
+static void test_matrix4f32_rotate_euler(void **state) {
+  (void)state; /* unused */
+
+  /* default attrubutes */
+  vector3f32_t angles = (vector3f32_t){{0, 0, 90}};
+  matrix4f32_t identity = (matrix4f32_t){
+      .column0 = {1, 0, 0, 0},
+      .column1 = {0, 1, 0, 0},
+      .column2 = {0, 0, 1, 0},
+      .column3 = {0, 0, 0, 1},
+  };
+
+  /* expected result */
+  matrix4f32_t expected = (matrix4f32_t){
+      .column0 = {0, 1, 0, 0},
+      .column1 = {-1, 0, 0, 0},
+      .column2 = {0, 0, 1, 0},
+      .column3 = {0, 0, 0, 1},
+  };
+
+  /* function call */
+  matrix4f32_t result = matrix4f32_rotate_euler(identity, angles);
 
   /* union assertions */
   assert_float_equal(result.x0, expected.x0, epsilon_f32);
@@ -195,6 +237,7 @@ int main(void) {
       cmocka_unit_test(test_matrix4f32_identity),
       cmocka_unit_test(test_matrix4f32_multiply),
       cmocka_unit_test(test_matrix4f32_translate),
+      cmocka_unit_test(test_matrix4f32_rotate_euler),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
