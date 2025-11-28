d95dc8a8a19da169bffb47abf9d895bfb995738e
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-11-28 06:53:03 -0300

    test(containers): add liked_list_t destroy and clear with callback unit test

--- tests/containers/linked_list.c
+++ tests/containers/linked_list.c
@@ -14,6 +14,14 @@ static b8 predicate_u64(void *data, void *item) {
   return value == item_value;
 }
 
+static b8 callback_u64(void *data, void *state) {
+  u64 *calls = ((u64 *)state);
+
+  *calls += 1;
+
+  return true;
+}
+
 static void test_linked_list_create_and_destroy(void **state) {
   (void)state; /* unused */
 
@@ -98,6 +106,48 @@ static void test_list_insert_and_remove(void **state) {
 
   /* list destruction */
   linked_list_destroy(list);
+}
+
+static void test_list_insert_and_destroy_with_callback(void **state) {
+  (void)state; /* unused */
+
+  /* default attributes */
+  const u64 stride = sizeof(u64);
+
+  /* default values */
+  const u64 item_to_insert_0 = 100;
+  const u64 item_to_insert_1 = 101;
+  const u64 item_to_insert_2 = 102;
+
+  /* default output variables */
+  u64 item_removed = 0;
+  u64 callback_calls = 0;
+
+  /* expected values */
+  const u64 count = 2;
+  const u64 callback_calls_count = 3;
+
+  /* list creation */
+  linked_list_t *list = linked_list_create(stride);
+
+  /* list insertion */
+  b8 insert_result_0 = linked_list_insert(list, 0, (void *)&item_to_insert_0);
+  b8 insert_result_1 = linked_list_insert(list, 1, (void *)&item_to_insert_1);
+  b8 insert_result_2 = linked_list_insert(list, 1, (void *)&item_to_insert_2);
+
+  /* insertion result assertion */
+  assert_true(insert_result_0);
+  assert_true(insert_result_1);
+  assert_true(insert_result_2);
+
+  /* list destruction */
+  b8 result = linked_list_destroy_with_callback(list, callback_u64, (void *)&callback_calls);
+
+  /* destruction result assertions */
+  assert_true(result);
+
+  /* callback calls assertions */
+  assert_int_equal(callback_calls, callback_calls_count);
 }
 
 static void test_linked_list_insert_and_retrieve(void **state) {
@@ -258,6 +308,7 @@ int main(void) {
       cmocka_unit_test(test_list_insert_and_remove),
       cmocka_unit_test(test_linked_list_create_and_destroy),
       cmocka_unit_test(test_linked_list_insert_and_retrieve),
+      cmocka_unit_test(test_list_insert_and_destroy_with_callback),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
