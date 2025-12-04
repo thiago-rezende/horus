b538b46dd9d5bd05c1b17593cbabafe6219b8219
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-12-04 08:24:17 -0300

    fix(filesystem|linux): use defualt libc error handling instead of the extensions provided on windows

--- horus/source/platform/linux/filesystem.c
+++ horus/source/platform/linux/filesystem.c
@@ -9,8 +9,6 @@
 #include <horus/platform/memory.h>
 #include <horus/platform/filesystem.h>
 
-#define FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE 255
-
 struct __platform_file {
   FILE *handle;
 };
@@ -18,15 +16,11 @@ struct __platform_file {
 platform_file_t *platform_file_open(char *path) {
   platform_file_t *file = platform_memory_allocate(sizeof(platform_file_t));
 
-  const errno_t error = fopen_s(&file->handle, path, "rb");
+  file->handle = fopen(path, "rb");
 
   if (file->handle == NULL) {
-    char buffer[FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE] = {0};
-
-    (void)strerror_s(buffer, FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE, error);
-
-    logger_critical_format("<path:%s> <code:%d> file opening failed", path, error);
-    logger_critical_format("|- [ error ] %s", buffer);
+    logger_critical_format("<path:%s> <code:%d> file opening failed", path, errno);
+    logger_critical_format("|- [ error ] %s", strerror(errno));
 
     platform_memory_deallocate(file);
 
@@ -37,7 +31,7 @@ platform_file_t *platform_file_open(char *path) {
 }
 
 b8 platform_file_close(platform_file_t *file) {
-  (void)fclose(file->handle);
+  fclose(file->handle);
 
   platform_memory_deallocate(file);
 
@@ -46,12 +40,8 @@ b8 platform_file_close(platform_file_t *file) {
 
 u64 platform_file_size(platform_file_t *file) {
   if (fseek(file->handle, 0, SEEK_END) != 0) {
-    char buffer[FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE] = {0};
-
-    (void)strerror_s(buffer, FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE, errno);
-
     logger_critical_format("<file:%p> <handle:%p> <code:%d> file seeking failed", file, file->handle, errno);
-    logger_critical_format("|- [ error ] %s", buffer);
+    logger_critical_format("|- [ error ] %s", strerror(errno));
 
     return 0;
   }
@@ -59,23 +49,15 @@ u64 platform_file_size(platform_file_t *file) {
   long size_in_bytes = ftell(file->handle);
 
   if (size_in_bytes == -1L && errno != 0) {
-    char buffer[FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE] = {0};
-
-    (void)strerror_s(buffer, FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE, errno);
-
     logger_critical_format("<file:%p> <handle:%p> <code:%d> file size retrieval failed", file, file->handle, errno);
-    logger_critical_format("|- [ error ] %s", buffer);
+    logger_critical_format("|- [ error ] %s", strerror(errno));
 
     return 0;
   }
 
   if (fseek(file->handle, 0, SEEK_SET) != 0) {
-    char buffer[FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE] = {0};
-
-    (void)strerror_s(buffer, FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE, errno);
-
     logger_critical_format("<file:%p> <handle:%p> <code:%d> file seeking failed", file, file->handle, errno);
-    logger_critical_format("|- [ error ] %s", buffer);
+    logger_critical_format("|- [ error ] %s", strerror(errno));
 
     return 0;
   }
@@ -87,12 +69,8 @@ u64 platform_file_read(platform_file_t *file, u8 *buffer, u64 size) {
   size_t bytes_read = fread(buffer, sizeof(u8), (size_t)size, file->handle);
 
   if (bytes_read != size) {
-    char buffer[FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE] = {0};
-
-    (void)strerror_s(buffer, FILESYSTEM_ERROR_MESSAGE_BUFFER_SIZE, errno);
-
     logger_critical_format("<file:%p> <handle:%p> <code:%d> file reading failed", file, file->handle, errno);
-    logger_critical_format("|- [ error ] %s", buffer);
+    logger_critical_format("|- [ error ] %s", strerror(errno));
 
     return 0;
   }
