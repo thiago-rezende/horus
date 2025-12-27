640f5162ed2e4964f7bd35ed51f6235a33bbf560
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-12-27 01:25:52 -0300

    feat(renderer|opengl): add instance buffer implementation

--- horus/include/horus/renderer/opengl/buffers/instance.h
+++ horus/include/horus/renderer/opengl/buffers/instance.h
@@ -3,4 +3,11 @@
 /* horus renderer buffers layer */
 #include <horus/renderer/buffers/instance.h>
 
-struct __instance_buffer {};
+/* horus renderer loader layer [ opengl ] */
+#include <horus/renderer/opengl/glad/gl.h>
+
+#define RENDERER_DEFAULT_INSTANCE_BUFFER_BINDING 1
+
+struct __instance_buffer {
+  GLuint instance_buffer;
+};
--- horus/source/renderer/opengl/buffers/instance.c
+++ horus/source/renderer/opengl/buffers/instance.c
@@ -8,40 +8,82 @@
 #include <horus/renderer/opengl/renderer.h>
 #include <horus/renderer/opengl/pipeline.h>
 
+/* horus renderer loader layer [ opengl ] */
+#include <horus/renderer/opengl/glad/gl.h>
+
 /* horus renderer buffers layer [ opengl ] */
 #include <horus/renderer/opengl/buffers/instance.h>
 
-#define DEFAULT_INSTANCE_BUFFER_BINDING 1
-
 instance_buffer_t *instance_buffer_create(renderer_t *renderer, instance_buffer_object_t *objects, u32 count) {
-  (void)count;    /* unused */
-  (void)objects;  /* unused */
   (void)renderer; /* unused */
+
+  GLuint instance_buffer = 0;
+
+  glGenBuffers(1, &instance_buffer);
+
+  if (instance_buffer == 0) {
+    logger_critical_format("<renderer:%p> uniform buffer creation failed", renderer);
+
+    return NULL;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, instance_buffer);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, RENDERER_DEFAULT_INSTANCE_BUFFER_BINDING, instance_buffer);
+
+  glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(instance_buffer_object_t) * count, NULL, GL_DYNAMIC_DRAW);
+
+  GLenum error = glGetError();
+
+  if (error != GL_NO_ERROR) {
+    logger_critical_format("<renderer:%p> instance buffer allocation failed", renderer);
+
+    glDeleteBuffers(1, &instance_buffer);
+
+    return NULL;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
   instance_buffer_t *buffer = platform_memory_allocate(sizeof(instance_buffer_t));
 
-  *buffer = (instance_buffer_t){};
+  *buffer = (instance_buffer_t){
+      .instance_buffer = instance_buffer,
+  };
 
   return buffer;
 }
 
 b8 instance_buffer_bind(instance_buffer_t *buffer, graphics_pipeline_t *pipeline, renderer_t *renderer) {
-  (void)buffer;   /* unused */
   (void)pipeline; /* unused */
   (void)renderer; /* unused */
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, buffer->instance_buffer);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, RENDERER_DEFAULT_INSTANCE_BUFFER_BINDING, buffer->instance_buffer);
 
   return true;
 }
 
 b8 instance_buffer_update(instance_buffer_t *buffer, instance_buffer_object_t *objects, u32 count) {
-  (void)count;   /* unused */
-  (void)buffer;  /* unused */
-  (void)objects; /* unused */
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, buffer->instance_buffer);
+
+  glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, sizeof(instance_buffer_object_t) * count, objects);
+
+  GLenum error = glGetError();
+
+  if (error != GL_NO_ERROR) {
+    logger_critical_format("<buffer:%p> instnace buffer data transfer failed", buffer);
+
+    return false;
+  }
+
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 
   return true;
 }
 
 b8 instance_buffer_destroy(instance_buffer_t *buffer) {
+  glDeleteBuffers(1, &buffer->instance_buffer);
+
   platform_memory_deallocate(buffer);
 
   return true;
